daa76a84e425411ce68b41a5cd1620b36ecb6b9d
Jonathan Allen <jallen@snow.edu>
2021-12-10 13:49:45 -0700

    Add swagger link

---
+++ HungryTests/GameHelper.cs
@@ -0,0 +1,43 @@
+﻿using HungryHippos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HungryTests
+{
+    public static class GameHelper
+    {
+        public static string DrawBoard(IEnumerable<Cell> cells)
+        {
+            var board = new StringBuilder();
+
+            var maxRow = cells.Max(c => c.Location.Row);
+            var maxCol = cells.Max(c => c.Location.Column);
+
+            for(int row = 0; row < maxRow; row++)
+            {
+                for(int col = 0; col < maxCol; col++)
+                {
+                    var cell = cells.Single(c => c.Location.Row == row && c.Location.Column == col);
+                    if(cell.IsPillAvailable)
+                    {
+                        board.Append("🌯");
+                    }
+                    else if(cell.OccupiedBy != null)
+                    {
+                        board.Append(cell.OccupiedBy.Id);
+                    }
+                    else
+                    {
+                        board.Append(' ');
+                    }
+                }
+                board.AppendLine();
+            }
+
+            return board.ToString();
+        }
+    }
+}
